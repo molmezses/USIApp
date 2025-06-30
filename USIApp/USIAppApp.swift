ba402e7243a476ca015b1706f0b1d7e682285553
molmezses <mustafaolmezses@gmail.com>
2025-06-30 23:00:41 +0300

    Initial Commit

---
+++ USIApp/USIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  USIAppApp.swift
+//  USIApp
+//
+//  Created by Mustafa Ölmezses on 30.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct USIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
