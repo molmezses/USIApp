062386322619e0fab5b73593a97c06a59d90a988
molmezses <mustafaolmezses@gmail.com>
2025-07-10 15:49:16 +0300

    Fetch Academician Info Func Creted And AcademicianInfo Model Added

--- USIApp/Core/Acedemician/Profile/ViewModel/ProfileViewModel.swift
+++ USIApp/Core/Acedemician/Profile/ViewModel/ProfileViewModel.swift
@@ -30,6 +30,36 @@ class ProfileViewModel: ObservableObject {
             }
     }
     
-
+    func fetchAcademicianInfo(byId id:String , completion: @escaping (Result<AcademicianInfo , Error>) -> Void){
+        
+        let docRef = Firestore.firestore()
+            .collection("AcademicianInfo")
+            .document(id)
+        
+        docRef.getDocument { document , error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let document = document , document.exists , let data = document.data() else {
+                completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey : "Belge bulunamadı"])))
+                return
+            }
+            
+            let info = AcademicianInfo(id: document.documentID,
+                                       email: data["Email"] as? String ?? "Hata Email AcademicianInfo",
+                                       unvan: data["Program"] as? String ?? "Hata Program AcademicianInfo",
+                                       program: data["Unvan"] as? String ?? "Hata Unvan AcademicianInfo",
+                                       photo: data["adSoyad"] as? String ?? "Hata adSoyad AcademicianInfo",
+                                       bolum: data["bolum"] as? String ?? "Hata bolum AcademicianInfo",
+                                       adSoyad: data["resimURL"] as? String ?? "Hata resimURL AcademicianInfo"
+            )
+            
+            completion(.success(info))
+            
+        }
+        
+    }
     
 }
