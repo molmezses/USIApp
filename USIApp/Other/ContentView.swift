5ec99020b3e995af7a6201af4b467056ee6b1d3c
molmezses <mustafaolmezses@gmail.com>
2026-01-05 21:58:03 +0300

    AuthService and ContentView Login pages new userRole enum updated

--- USIApp/Other/ContentView.swift
+++ USIApp/Other/ContentView.swift
@@ -10,46 +10,30 @@ struct ContentView: View {
     
     @EnvironmentObject var authViewModel: AuthViewModel
     @EnvironmentObject var requestVm: RequestIndustryViewModel
-    
-    @State private var academDomains: [String] = []
-    @State private var studentDomains: [String] = []
-    @State private var isStudent: Bool? = nil
+  
     
     var body: some View {
-        ZStack {
+        ZStack{
             if authViewModel.userSession == nil {
                 LoginView()
-            } else if let isStudent = isStudent {
-
-                let email = authViewModel.userSession?.email ?? ""
-                let domain = UserDomainService.shared.domainFromEmail(email)
-
-                if isStudent {
-                    StudentTabView(selectedTab: 0)
-                } else if academDomains.contains(domain) {
-                    AcademicianTabView(selectedTab: 1)
-                } else {
+            }else if let session = authViewModel.userSession{
+                
+                switch session.role {
+                case .industry:
                     IndustryTabView(selectedTab: 0)
                         .environmentObject(requestVm)
+                case .academician:
+                    AcademicianTabView(selectedTab: 1)
+                case .student:
+                    StudentTabView(selectedTab: 0)
+                case .unkown:
+                    ProgressView("Lütfen bekleyiniz..")
                 }
-
-            } else {
-                ProgressView() // rol
+                
             }
         }
-        .animation(.easeInOut(duration: 0.35), value: authViewModel.userSession)
-        .task(id: authViewModel.userSession?.id) {
-            guard let email = authViewModel.userSession?.email else { return }
-
-            do {
-                isStudent = nil
-                academDomains = try await UserDomainServiceContent.shared.fetchAcademicianDomains()
-                isStudent = try await UserDomainServiceContent.shared.isStudent(email: email)
-            } catch {
-                isStudent = false
-            }
-        }
-
     }
+    
+    
 }
 
